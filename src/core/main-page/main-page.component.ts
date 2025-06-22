22ebfdb1863a4646c1cfbfb2e15d6f1f070b14b2
CharalamposTsagkalidis <bill1.dim2@gmail.com>
2025-06-22 11:10:49 +0300

    fix for sending strings and not trims numbers

--- src/core/main-page/main-page.component.ts
+++ src/core/main-page/main-page.component.ts
@@ -13,15 +13,17 @@ import { debounceTime, distinctUntilChanged, Subject } from "rxjs";
 
 export class MainPageComponent {
     private http = inject(HttpClient);
+    private  url = "http://localhost:11000/api/regnonize-numbers/service/numbers";
+    // This is the input field for numbers
     numberInput= '';
 
     private inputChange= new Subject<string>();
-    numbers: number[] = [];
+    numbers: string[] = [];
     constructor() {
         this.inputChange
             .pipe(
                 // Add any operators you need here, e.g., debounceTime, distinctUntilChanged, etc.
-                debounceTime(300), // Wait for 300ms pause in events
+                debounceTime(1000), // Wait for 300ms pause in events
                  // Only emit if the current value is different than the last
                  distinctUntilChanged()
                 )
@@ -33,15 +35,21 @@ export class MainPageComponent {
     }
 
     onInputChange(value: string) {
+        console.log("Input changed:", value);
+        this.numberInput = value; // Update the input field value
+        console.log("Updated numberInput:", this.numberInput);
         this.inputChange.next(value);
     }
 
+
     processAndSend(input:string) {
-        const nums = input.split(",").map(num => Number(num.trim)).filter(num => !isNaN(num));
-        
+        //const nums = input.split(",").map(num => Number(num.trim())).filter(num => !isNaN(num));
+        const nums = input.split(",");
+        //this.numbers = [nums].flat();
         this.numbers = nums;
-        
-        this.http.post("http://localhost:3000/numbers",nums).subscribe({
+        console.log("Processed numbers:", this.numbers);
+      
+        this.http.post(this.url,{numbers:this.numbers}).subscribe({
             next: (response) => {
                 console.log("Numbers sent successfully:", response);
             },
@@ -51,6 +59,4 @@ export class MainPageComponent {
         });
    
     }
-
-    
 }
